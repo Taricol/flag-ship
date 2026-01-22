4fa7c9746b06567b744573eb85e18e59f3070a25
Taricol <mozumder9122023@gmail.com>
2026-01-22 11:55:15 +0600

    Navbar added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import bannerImg from '../assets/banner.png'
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className='py-12'>
+            <img src={bannerImg} alt="Banner Image" className='w-full mx-auto md:max-w-md' />
         </div>
     );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
         <>
             <Navbar></Navbar>
-            <Outlet/>
-            <h1>Footer</h1>
+            <div className='min-h-[calc(100vh-116px)]'>
+                <div className='max-w-screen-2xl mx-auto px-8 md:px-12 lg:px-16 xl:px-24'>
+                    <Outlet/>
+                </div>
+            </div>
+            <Footer></Footer>
         </>
     );
 };
