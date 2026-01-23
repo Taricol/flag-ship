4561190d91da93586af3a39eee5fff9853535a6a
Taricol <mozumder9122023@gmail.com>
2026-01-23 13:07:29 +0600

    bookmarked added

--- src/pages/PhoneDetails.jsx
+++ src/pages/PhoneDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import Button from '../Components/ui/Button';
 import { MdBookmarkAdd,MdShoppingCartCheckout} from "react-icons/md";
+import { addFavorite } from '../utils';
 const PhoneDetails = () => {
     const data=useLoaderData()
     const {id}=useParams()
@@ -17,16 +18,24 @@ const PhoneDetails = () => {
         camera_info,
     }=singlePhone || {}
     
+    const handleFavorite=()=>{
+        addFavorite(singlePhone)
+    }
+
+
     return (
         <div className='w-full py-12'>
             <img src={image} alt="Banner Image" className='w-full mx-auto md:w-auto mb-8' />
             <div className="flex justify-between">
                 <h1 className='text-6xl font-thin mb-8'>{name}</h1>
-                <div>
+                <div className='space-x-4'>
                     <Button label={<MdShoppingCartCheckout/>}></Button>
-                    <Button label={<MdBookmarkAdd/>}></Button>
+                    <Button onClick={handleFavorite} label={<MdBookmarkAdd/>}></Button>
                 </div>
             </div>
+
+            {/* details */}
+            <div></div>
         </div>
     );
 };
